bc70667627a9e6e8b04259a6f48e9ade8c16be18
nogno <69785934+nogno@users.noreply.github.com>
2022-06-04 14:46:00 +0900

    Update GPS_SoftUART_test.py
    utf-8エラー対策として例外処理を加えた

--- test/GPS/GPS_SoftUART_test.py
+++ test/GPS/GPS_SoftUART_test.py
@@ -11,11 +11,14 @@ def rungps(): # GPSモジュールを読み、GPSオブジェクトを更新す�
     s = serial.Serial('/dev/ttySOFT0', 9600, timeout=10)
     s.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
     while True:
-        sentence = s.readline().decode('utf-8') # GPSデーターを読み、文字列に変換する
-        if sentence[0] != '$': # 先頭が'$'でなければ捨てる
-            continue
-        for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
-            gps.update(x)
+        try:
+            sentence = s.readline().decode('utf-8') # GPSデーターを読み、文字列に変換する
+            if sentence[0] != '$': # 先頭が'$'でなければ捨てる
+                continue
+            for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
+                gps.update(x)
+        except:
+            print("GPS error, but ignored")
 
 gpsthread = threading.Thread(target=rungps, args=()) # 上の関数を実行するスレッドを生成
 gpsthread.daemon = True
