579dccbe2808f51cc55e7a5c2365f2e00e2b138d
nogno <69785934+nogno@users.noreply.github.com>
2022-06-09 19:57:44 +0900

    Update GPS_Softuart_for_debug.py

--- test/GPS/GPS_Softuart_for_debug.py
+++ test/GPS/GPS_Softuart_for_debug.py
@@ -8,21 +8,21 @@ gps = micropyGPS.MicropyGPS(9, 'dd') # MicroGPSオブジェクトを生成する
                                     # 引数はタイムゾーンの時差と出力フォーマット
 
 def rungps(): # GPSモジュールを読み、GPSオブジェクトを更新する
-    s = serial.Serial('/dev/ttySOFT0', 4800, timeout=10)
-    s.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
     while True:
         try:
-            print(str(s.readline()))
-            sentence = s.readline().decode('utf-8') # GPSデーターを読み、文字列に変換する
-            if sentence[0] != '$': # 先頭が'$'でなければ捨てる
-                continue
-            for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
-                gps.update(x)
-        except:
-            print("GPS error, but ignored")
+            s = serial.Serial('/dev/serial0', 9600, timeout=10)
+            s.readline() # 最初の1行は中途半端なデーターが読めることがあるので、捨てる
+            while True:
+                sentence = s.readline().decode('cp1252') # GPSデーターを読み、文字列に変換する
+                if sentence[0] != '$': # 先頭が'$'でなければ捨てる
+                    continue
+                for x in sentence: # 読んだ文字列を解析してGPSオブジェクトにデーターを追加、更新する
+                    gps.update(x)
+        except Exception as e: #例外処理
+            print(e)
 
 gpsthread = threading.Thread(target=rungps, args=()) # 上の関数を実行するスレッドを生成
-gpsthread.daemon = True
+# gpsthread.daemon = True
 gpsthread.start() # スレッドを起動
 
 while True:
