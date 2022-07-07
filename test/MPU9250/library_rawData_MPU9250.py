626317156b8dede69b01f6a83d201bd314f3b98e
nogno <69785934+nogno@users.noreply.github.com>
2022-07-07 12:55:54 +0900

    Update library_rawData_MPU9250.py

--- test/MPU9250/library_rawData_MPU9250.py
+++ test/MPU9250/library_rawData_MPU9250.py
@@ -18,12 +18,12 @@ mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
 try:
     while True:
-        accel = mpu9250.readAccel()
+#         accel = mpu9250.readAccel()
 #         print(" ax = " , ( accel['x'] ))
 #         print(" ay = " , ( accel['y'] ))
 #         print(" az = " , ( accel['z'] ))
 
-        gyro = mpu9250.readGyro()
+#         gyro = mpu9250.readGyro()
 #         print(" gx = " , ( gyro['x'] ))
 #         print(" gy = " , ( gyro['y'] ))
 #         print(" gz = " , ( gyro['z'] ))
