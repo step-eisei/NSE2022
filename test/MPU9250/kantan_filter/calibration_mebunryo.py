4b657b3a7961a25f84d74d0f3e220e1b75077bec
nogno <69785934+nogno@users.noreply.github.com>
2022-07-08 17:04:53 +0900

    Update calibration_mebunryo.py

--- test/MPU9250/kantan_filter/calibration_mebunryo.py
+++ test/MPU9250/kantan_filter/calibration_mebunryo.py
@@ -89,9 +89,9 @@ try:
  
         
         # とりあえずatan2に入れたものをtheta_absoluteとしているが，本当に欲しいtheta_absoluteにするには演算が必要かも
-        theta_absolute = atan2(magY_calibrated, magX_calibrated)
+        theta_absolute = math.atan2(magY_calibrated, magX_calibrated)
         # print(theta_absolute)
-        theta_absolute_lowPass = atan2(magY_mean, magX_mean)
+        theta_absolute_lowPass = math.atan2(magY_mean, magX_mean)
         print(theta_absolute_lowPass)
 
         with open('result/' + csv_name,'a',newline='') as f: 
