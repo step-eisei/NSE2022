d487e9879e7da23e8bd9b804a99cdc0553891199
STEPeisei <step.eisei@gmail.com>
2022-07-08 14:49:34 +0900

    pushed by noguchi

---
+++ test/TWELITE/serial_T2.py
@@ -0,0 +1,24 @@
+import serial
+import time
+from struct import *
+
+COM = '/dev/ttyAMA0'
+
+ser = serial.Serial(COM, 115200)   
+
+x=109
+# Y=209
+# Z=98
+
+
+
+while True:
+
+    # data = [x,y,z]
+    data = x
+    ser.write(data)
+    print("send")
+
+    time.sleep(1)
+    
+ser.close()          
