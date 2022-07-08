572f37d75a50feaff0d51168ef2bc7f404b52d65
STEPeisei <step.eisei@gmail.com>
2022-07-08 17:22:12 +0900

    by okada

--- test/TWELITE/send_data.py
+++ test/TWELITE/send_data.py
@@ -2,21 +2,29 @@ import serial
 import time
 from struct import *
 
+
 COM = '/dev/ttyAMA0'
 
+
 ser = serial.Serial(COM, 115200)   
+
 
 X=109.243
 Y=-209
 Z=3098
 
+
 while True:
+
 
     data = pack('>ddd', X, Y, Z)
     print(data)
+    data = data + b'\n'
     ser.write(data)
     print("send")
 
+
     time.sleep(1)
     
-ser.close() 
+ser.close()  
+
