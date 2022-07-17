12151ba71afb6b74f4f535dc48a9a9f5c51e7a65
nogno <69785934+nogno@users.noreply.github.com>
2022-07-17 17:09:44 +0900

    Update motor_test_noLibrary.py

--- test/motor/motor_test_noLibrary.py
+++ test/motor/motor_test_noLibrary.py
@@ -28,7 +28,7 @@ GPIO.setup(PIN_BIN2, GPIO.OUT)
 
 # 右モータPWM
 GPIO.setup(PIN_PWMB, GPIO.OUT)
-pwm_right = GPIO.PWM(PIN_PWMB, 50)
+pwm_right = GPIO.PWM(PIN_PWMB, DUTY_B)
 pwm_right.start(10)
 pwm_right.ChangeDutyCycle(DUTY_B)
 
