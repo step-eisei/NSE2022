15a5b32159f5d80635ee27c9dd326da2bbd48108
mayukoogi <89843480+mayukoogi@users.noreply.github.com>
2022-07-21 23:16:13 +0900

    Update motor_test_noLibrary.py

--- test/motor/motor_test_noLibrary.py
+++ test/motor/motor_test_noLibrary.py
@@ -7,10 +7,10 @@ PIN_PWMA = 12
 PIN_BIN1 = 16
 PIN_BIN2 = 26
 PIN_PWMB = 13
-DUTY_A = 20 # 念のため20より上には上げないように
-DUTY_B = 20 # 念のため20より上には上げないように
+DUTY_A = 5 # 念のため20より上には上げないように
+DUTY_B = 5 # 念のため20より上には上げないように
 
-GPIO.setmode(GPIO.BCM)
+GPIO.smode(GPIO.BCM)
 
 # 左モータ
 GPIO.setup(PIN_AIN1, GPIO.OUT)
