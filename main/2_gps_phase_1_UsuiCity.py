1fd52d2fbaf11192c593b7e822faec80965e0c2c
nogno <69785934+nogno@users.noreply.github.com>
2022-07-26 23:22:07 +0900

    Update 2_gps_phase_1_UsuiCity.py

--- main/2_gps_phase_1_UsuiCity.py
+++ main/2_gps_phase_1_UsuiCity.py
@@ -25,8 +25,9 @@ mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 #     line = [row for row in reader]
 #     goal_latitude = float(line[ 1 ] [ 0 ])
 #     goal_longitude = float(line[ 1 ] [ 1 ])
-goal_latitude = 36.1119
-goal_longitude = 140.09862666666666
+goal_latitude = 36.11184166666666
+goal_longitude = 140.09862166666667
+
 
 # モータのピン割り当て(GPIO 〇〇)
 PIN_AIN1 = 24   # 右モータ(A)
