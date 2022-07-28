40933301158dfce3f722ea56812c7d989cdbdca2
kaise-m <90245285+kaise-m@users.noreply.github.com>
2022-07-28 21:24:51 +0900

    ピン番号変更（１７⇒２２）

--- EndtoEnd/guide_phase.py
+++ EndtoEnd/guide_phase.py
@@ -87,12 +87,12 @@ magYs = [0]*5
 
 def nchrm(): #ニクロム線加熱
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17,  GPIO.OUT)
+    GPIO.setup(22,  GPIO.OUT)
 
-    GPIO.output(17, True)
+    GPIO.output(22, True)
     #ここの数字は実験次第
     time.sleep(2)
-    GPIO.output(17, False)
+    GPIO.output(22, False)
     time.sleep(5)
 
 def get_pressure():
