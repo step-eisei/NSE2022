2ee59804ee69ced10807a71594877488e06b06f5
shota182 <104834704+shota182@users.noreply.github.com>
2022-08-01 22:01:42 +0900

    Update Calibration.py
    gpsとmpu9250のキャリブレーションをするプログラムです．08/01

--- EndtoEnd/Calibration.py
+++ EndtoEnd/Calibration.py
@@ -29,6 +29,13 @@ import RPi.GPIO as GPIO
 #GPSモジュールからデータをmy_gps(GPSオブジェクト)に追加，＊引数はタイムゾーンの時差（日本は＋9時間）と，経度緯度の出力フォーマットを指定（ddm,dms,ddなどから）
 my_gps = micropyGPS.MicropyGPS(9, 'dd')
 
+def percentpick(listdata):
+    n = int(len(listdata) *p/100)
+    listdata = sorted(listdata) # 昇順
+    min = listdata[n-1]
+    max = listdata[len(listdata)-n]
+    return max, min
+
 #GPSモジュールを読み，my_gpsを更新する関数
 def rungps():
     #シリアルポートの指定，シリアル通信の速度[bps]を指定
@@ -50,12 +57,32 @@ gpsthread = threading.Thread(target=rungps, args=())
 gpsthread.daemon = True
 #スレッドを起動
 gpsthread.start()
+'''
+以下3行に渡ってcsvファイル名を作成している．
+datetimeで得られる現在時刻にはスペース，コロン，ピリオドが含まれており，
+これはLinuxでは保存できるがWindowsでは保存できないため，
+それらの文字をreplaceでアンダーバーやハイフンに置換している．
+もっといい方法があると思うが，とりあえずこれで実装した．
+'''
+DIFF_JST_FROM_UTC = 9
+jp_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+gps_name = 'calibration_gps/gps_record_' + str(jp_time).replace(' ', '_').replace(':', '-').replace('.', '_') + '.csv'
+mag_name = 'calibration_9axis/mag_record_' + str(jp_time).replace(' ', '_').replace(':', '-').replace('.', '_') + '.csv'
+# print(jp_time)
+# >> 2022-07-07 13:28:32.197486
+# print(csv_name)
+# >> mag_record_2022-07-07_13-28-32_197156
 
 #引数には（保存先のパス，読み書きの指定やバイナリ，テキストの指定はmodeを使う,）
-with open('goal_gps.csv',mode='w',newline='') as f:
+with open(gps_name,'w',newline='') as f:
     writer = csv.writer(f)
     #一行書き込み
     writer.writerow(["goal_latitude", "goal_longitude"])
+
+with open(mag_name,'w',newline='') as f: 
+    writer = csv.writer(f)
+    writer.writerow(["magX", "magY", "magZ"])
+f.close()
 
 #10回データを書き込めば終了
 gps_latitude = 100
@@ -72,9 +99,9 @@ for i in range(10):
                 sum_latitude += gps_latitude
                 sum_longitude += gps_longitude
                 #衛星数を出力
-                print("num =", my_gps.satellites_used)
+                print(f"gps_latitude = {gps_latitude}, gps_longitude = {gps_longitude}")
                 #mode='a'は追記モードでファイルを開く
-                with open('goal_gps.csv',mode='a',newline='') as f:
+                with open(gps_name,mode='a',newline='') as f:
                     writer = csv.writer(f)
                     writer.writerow([gps_latitude,gps_longitude]) 
                 print("data add")
@@ -94,35 +121,6 @@ with open('goal.csv',mode='a',newline='') as f:
 
 # ここから9軸
 mpu9250 = FaBo9Axis_MPU9250.MPU9250()
-
-# 以下，目分量で得られた最大値と最小値
-magX_max = 35.5
-magX_min = -6.9
-magY_max = -385.3
-magY_min = -430.7
-
-magXs = [0]*4
-magYs = [0]*4
-
-'''
-以下3行に渡ってcsvファイル名を作成している．
-datetimeで得られる現在時刻にはスペース，コロン，ピリオドが含まれており，
-これはLinuxでは保存できるがWindowsでは保存できないため，
-それらの文字をreplaceでアンダーバーやハイフンに置換している．
-もっといい方法があると思うが，とりあえずこれで実装した．
-'''
-DIFF_JST_FROM_UTC = 9
-jp_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-csv_name = 'mag_record_calib_mebunryo_' + str(jp_time).replace(' ', '_').replace(':', '-').replace('.', '_') + '.csv'
-# print(jp_time)
-# >> 2022-07-07 13:28:32.197486
-# print(csv_name)
-# >> mag_record_2022-07-07_13-28-32_197156
-
-with open('9axis_rawdata/' + csv_name,'w',newline='') as f: 
-    writer = csv.writer(f)
-    writer.writerow(["magX", "magY", "magZ", "magX_calibrated", "magY_calibrated", "theta_absolute", "magX_mean", "magY_mean", "theta_absolute_lowPass"])
-f.close()
 
 try:
     magX_save = []
@@ -160,8 +158,6 @@ try:
     # sleep
     time.sleep(2)
     print("set up finished")
-    pwm_left.ChangeDutyCycle(1)
-    pwm_right.ChangeDutyCycle(1)
     # 左に旋回
     # 右モータ前進
     GPIO.output(PIN_AIN1, GPIO.LOW)
@@ -169,54 +165,24 @@ try:
     # 左モータ後進
     GPIO.output(PIN_BIN1, GPIO.LOW)
     GPIO.output(PIN_BIN2, GPIO.HIGH)
+    magx = []
+    magy = []
     while True:
-#         accel = mpu9250.readAccel()
-#         print(" ax = " , ( accel['x'] ))
-#         print(" ay = " , ( accel['y'] ))
-#         print(" az = " , ( accel['z'] ))
-
-#         gyro = mpu9250.readGyro()
-#         print(" gx = " , ( gyro['x'] ))
-#         print(" gy = " , ( gyro['y'] ))
-#         print(" gz = " , ( gyro['z'] ))
-
+        pwm_left.ChangeDutyCycle(1)
+        pwm_right.ChangeDutyCycle(1)
+        time.sleep(0.2)
+        pwm_left.ChangeDutyCycle(INITIAL_DUTY_A)
+        pwm_right.ChangeDutyCycle(INITIAL_DUTY_B)
+        time.sleep(0.2)
         mag = mpu9250.readMagnet()
-#         print(" mx = " , ( mag['x']   ), end='')
-#         print(" my = " , ( mag['y']   ), end='')
-#         print(" mz = " , ( mag['z'] ))
-#         print()
-           
-        # キャリブレーション
-        magX_calibrated = (mag['x']-(magX_max + magX_min)/2) / ((magX_max - magX_min)/2)
-        magY_calibrated = (mag['y']-(magY_max + magY_min)/2) / ((magY_max - magY_min)/2)
-        
-        # ローパスフィルタ
-        magXs.append(magX_calibrated)
-        magX_mean = sum(magXs)/5
-        del magXs[0]
-        
-        
-        magYs.append(magY_calibrated)
-        magY_mean = sum(magYs)/5
-        del magYs[0]
- 
-        
-        # とりあえずatan2に入れたものをtheta_absoluteとしているが，本当に欲しいtheta_absoluteにするには演算が必要かも
-        theta_absolute = math.atan2(-magY_calibrated, -magX_calibrated)*180/math.pi
-        # print(theta_absolute)
-        theta_absolute_lowPass = math.atan2(-magY_mean, -magX_mean)*180/math.pi
-        print(theta_absolute_lowPass)
-        
-        # dataの抜き出し
-        magX_save.append(float(mag['x']))
-        magY_save.append(float(mag['y']))
-      
-        with open('9axis_rawdata/' + csv_name,'a',newline='') as f: 
+        magx.append(mag['x'])
+        magy.append(mag['y'])
+        print(f"x = {mag['x']}, y = {mag['y']}")
+        with open(mag_name,'a',newline='') as f: 
             writer = csv.writer(f)
-            writer.writerow([mag['x'], mag['y'], mag['z'], magX_calibrated, magY_calibrated, theta_absolute, magX_mean, magY_mean, theta_absolute_lowPass])
+            writer.writerow([mag['x'], mag['y'], mag['z']])
         f.close()
-         
-        time.sleep(0.3)
+        
     
 except KeyboardInterrupt:
     pwm_left.ChangeDutyCycle(INITIAL_DUTY_A)
@@ -225,13 +191,17 @@ except KeyboardInterrupt:
     pwm_right.stop()
     GPIO.cleanup()
     # 最大値，最小値の算出
-    magX_max = max(magX_save)
-    magX_min = min(magX_save)
-    magY_max = max(magY_save)
-    magY_min = min(magY_save)
-    with open('9axis_rawdata/mag_record_calib_mebunryo_max_min.csv', 'w', newline='') as f:
+    p = 3 # 上位何%をpickするか
+    Xmax, Xmin = percentpick(magx)
+    Ymax, Ymin = percentpick(magy)
+    Xcenter = (Xmax + Xmin) / 2
+    Ycenter = (Ymax + Ymin) / 2
+    with open('mag.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['magX_max', 'magX_min', 'magY_max', 'magY_min'])
-        writer.writerow([magX_max, magX_min, magY_max, magY_min])
+        f.close()
+    with open('mag.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([Xmax, Xmin, Ymax, Ymin])
         f.close()
     sys.exit()
