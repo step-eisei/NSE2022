b90082c20563602cf724637f4e2ec6869105226e
nogno <69785934+nogno@users.noreply.github.com>
2022-08-06 18:11:14 +0900

    Update calibration_camera.py

--- main/camera_phase/calibration_camera.py
+++ main/camera_phase/calibration_camera.py
@@ -30,6 +30,30 @@ def sv_scan(img_hsv):
     val_avg = np.average(im_v)
     
     return sat_avg,val_avg
+
+def hsv_binary(img_hsv,sat_avg,val_avg):
+    #画像のチャンネルを分ける
+    #画像のチャンネルを分ける img[縦，横，[h,s,v]]　この行程でグレースケールになる
+    im_h= img_hsv[:, :, 0] + 0*img_hsv[:, :, 1] + 0*img_hsv[:, :, 2] # h以外の情報を消去してim_hに格納
+    im_s= 0*img_hsv[:, :, 0] + img_hsv[:, :, 1] + 0*img_hsv[:, :, 2] # s以外の情報を消去してim_sに格納
+    im_v= 0*img_hsv[:, :, 0] + 0*img_hsv[:, :, 1] + img_hsv[:, :, 2] # v以外の情報を消去してim_vに格納
+
+
+    # 条件を満たしていれば1, それ以外は0に置換
+    # np.where(条件, Trueの時に置換する数, Falseの時に置換する数)
+    #色相環は360度→0～255に変換
+    #色相環の内、赤色は0度と360度をまたぐ
+    img_h_th = np.where((im_h < 15/360*255) | (im_h > 345/360*255), 1, 0)
+    img_s_th = np.where(im_s > sat_avg*0.9, 1, 0)
+    img_v_th = np.where(im_v > val_avg*0.9, 1, 0)
+
+    #行列の掛け算ではなく各要素の掛け算をする 上記の条件で一つでも満たしていないものがあれば，0となる．
+    #検出された物を白にするために最後に255を掛ける(この時点で2値化)
+    img_th = img_h_th * img_s_th * img_v_th * 255 # 条件を満たした要素は255，それ以外は0
+
+    img_th = np.uint8(img_th) # np.uint8 unsigned int (0 ～ 255)
+
+    return img_th
   
 # 撮影
 now_time_camera = datetime.datetime.now()
