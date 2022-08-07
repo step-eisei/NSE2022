6b7657dff465641c893abe1a73b8e3c8e0153416
shota182 <104834704+shota182@users.noreply.github.com>
2022-08-07 17:24:12 +0900

    Update go_ahead_check.py

--- EndtoEnd/go_ahead_check.py
+++ EndtoEnd/go_ahead_check.py
@@ -1,4 +1,4 @@
-# モータの前進のみを行い，反転できるか試験するプログラム
+# モータによる前後進能力，回転能力を把握するプログラム
 
 import RPi.GPIO as GPIO
 import math
@@ -78,24 +78,6 @@ def go_ahead():
 
 # 機体を後進，急停止させる関数
 def go_stop():
-#     # モータのセッティング
-#     GPIO.setmode(GPIO.BCM)
-#     # 左モータ
-#     GPIO.setup(PIN_AIN1, GPIO.OUT)
-#     GPIO.setup(PIN_AIN2, GPIO.OUT)
-#     # 左モータPWM
-#     GPIO.setup(PIN_PWMA, GPIO.OUT)
-#     pwm_left = GPIO.PWM(PIN_PWMA, freq)
-#     pwm_left.start(10)
-#     # 右モータ
-#     GPIO.setup(PIN_BIN1, GPIO.OUT)
-#     GPIO.setup(PIN_BIN2, GPIO.OUT)
-#     # 右モータPWM
-#     GPIO.setup(PIN_PWMB, GPIO.OUT)
-#     pwm_right = GPIO.PWM(PIN_PWMB, freq)
-#     pwm_right.start(10)
-#     # sleep
-#     time.sleep(2)
     # 右モータ後進
     GPIO.output(PIN_AIN1, GPIO.HIGH)
     GPIO.output(PIN_AIN2, GPIO.LOW)
@@ -125,12 +107,99 @@ def go_stop():
             pwm_right.ChangeDutyCycle((100-i)*DUTY_B/200)
             time.sleep(0.07)
     time.sleep(2)
-    # モータの解放
-#     pwm_right.stop()
-#     pwm_left.stop()
-#     GPIO.cleanup()
 
+def go_stop2(): # 前進速め
+    # 右モータ後進
+    GPIO.output(PIN_AIN1, GPIO.HIGH)
+    GPIO.output(PIN_AIN2, GPIO.LOW)
+    # 左モータ後進
+    GPIO.output(PIN_BIN1, GPIO.LOW)
+    GPIO.output(PIN_BIN2, GPIO.HIGH)
+    # 0からDUTYまで数秒かけて上げる
+    for i in range(0, 101, 5):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/100)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/200)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/200)
+            time.sleep(0.07)
+    # sleep
+    time.sleep(T_straight)
+    # DUTYから0まで数秒かけて下げる
+    for i in range(0, 101, 50):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/100)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/200)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/200)
+            time.sleep(0.07)
+    time.sleep(2)
 
+def go_stop3(): # 急前進
+    # 右モータ後進
+    GPIO.output(PIN_AIN1, GPIO.HIGH)
+    GPIO.output(PIN_AIN2, GPIO.LOW)
+    # 左モータ後進
+    GPIO.output(PIN_BIN1, GPIO.LOW)
+    GPIO.output(PIN_BIN2, GPIO.HIGH)
+    # 0からDUTYまで数秒かけて上げる
+    for i in range(0, 101, 50):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/100)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/200)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/200)
+            time.sleep(0.07)
+    # sleep
+    time.sleep(T_straight)
+    # DUTYから0まで数秒かけて下げる
+    for i in range(0, 101, 2):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/100)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/200)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/200)
+            time.sleep(0.07)
+    time.sleep(2)
+
+def go_stop4(): # 急前進，減速速め
+    # 右モータ後進
+    GPIO.output(PIN_AIN1, GPIO.HIGH)
+    GPIO.output(PIN_AIN2, GPIO.LOW)
+    # 左モータ後進
+    GPIO.output(PIN_BIN1, GPIO.LOW)
+    GPIO.output(PIN_BIN2, GPIO.HIGH)
+    # 0からDUTYまで数秒かけて上げる
+    for i in range(0, 101, 50):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/100)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle(i*DUTY_A/200)
+            pwm_right.ChangeDutyCycle(i*DUTY_B/200)
+            time.sleep(0.07)
+    # sleep
+    time.sleep(T_straight)
+    # DUTYから0まで数秒かけて下げる
+    for i in range(0, 101, 5):
+        if(math.sqrt( x_now**2 + y_now**2 ) > 7): 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/100)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/100)
+            time.sleep(0.1)
+        else: 
+            pwm_left.ChangeDutyCycle((100-i)*DUTY_A/200)
+            pwm_right.ChangeDutyCycle((100-i)*DUTY_B/200)
+            time.sleep(0.07)
+    time.sleep(2)
 
 # motorをセットアップする
 INITIAL_DUTY_A = 0
