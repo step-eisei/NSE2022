57544c840de975387ee01424cc48d2af1970fbac
kaise-m <90245285+kaise-m@users.noreply.github.com>
2022-08-12 09:21:42 +0900

    Create nikuromu_several_times.py

---
+++ test/nikuromu/nikuromu_several_times.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22,  GPIO.OUT)
+
+for i in range(4):
+  GPIO.output(22, True)
+  print("nchrm "+str(i))
+  time.sleep(2)
+  GPIO.output(22, False)
+  time.sleep(5)
+
+GPIO.cleanup()
