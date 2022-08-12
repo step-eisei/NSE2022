16466ca824271c3cb56671ae3e9e8df58871d7c3
kaise-m <90245285+kaise-m@users.noreply.github.com>
2022-08-12 09:24:43 +0900

    Update nikuromu_several_times.py

--- test/nikuromu/nikuromu_several_times.py
+++ test/nikuromu/nikuromu_several_times.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+for:
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(22,  GPIO.OUT)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(22,  GPIO.OUT)
+  for i in range(4):
+    GPIO.output(22, True)
+    print("nchrm "+str(i))
+    time.sleep(2)
+    GPIO.output(22, False)
+    time.sleep(5)
 
-for i in range(4):
-  GPIO.output(22, True)
-  print("nchrm "+str(i))
-  time.sleep(2)
-  GPIO.output(22, False)
-  time.sleep(5)
-
-GPIO.cleanup()
+  GPIO.cleanup()
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  print("cleanup fin.")
